ec27a9c294f5d89745aacbaeac11eff159e0ab7d
Claire Happel <clairehap1@gmail.com>
2026-03-04 10:58:06 -0600

    finished fizz buzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,8 +10,15 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
  */
+// I: none
+// O: none
+// C: console log either a number, fizz, buzz, or fizzbuzz depending on what the number is divisible by
 function fizzBuzz() {
-  // YOUR CODE HERE //
+// for start 1, stop 100, increment 1
+for (let i = 1; i <= 100; i++){
+  // log to console Fizz if i is divisble by 3, plus Buzz if it is divisible by 5 or log the number
+  console.log((i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
+}
 }
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
